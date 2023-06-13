3ac5c415fa143a74b32fffa7445c4a239c04aff4
Aihara <aihara-sama@mail.ru>
2023-06-13 16:10:57 +0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,54 @@
+import type { EmotionCache } from "@emotion/react";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import Auth from "components/auth/Auth";
+import { Meta } from "components/common/Meta";
+import { Toaster } from "components/common/Toaster";
+import { ThemeProvider } from "contexts/theme";
+import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Router } from "next/router";
+import nProgress from "nprogress";
+import { Provider } from "react-redux";
+import { store } from "store";
+import createEmotionCache from "utils/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Study Guru</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Meta />
+      <Provider store={store}>
+        <ThemeProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <CssBaseline />
+            <Toaster />
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          </LocalizationProvider>
+        </ThemeProvider>
+      </Provider>
+    </CacheProvider>
+  );
+}
+
+export default appWithTranslation(MyApp);
